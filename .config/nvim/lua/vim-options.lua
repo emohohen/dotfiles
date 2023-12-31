041803de47c46ed6cad1ef24c388668d707ddad4
daniel <dps117@gmail.com>
2023-12-30 21:35:15 -0500

    simplify

---
+++ .config/nvim/lua/vim-options.lua
@@ -0,0 +1,42 @@
+
+vim.o.guicursor = ""
+
+vim.o.smartindent = true
+vim.o.wrap = false
+
+vim.o.colorcolumn = "80"
+
+vim.o.hlsearch = false
+vim.o.infercase = true
+
+vim.o.number = true
+vim.o.relativenumber = true
+
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = 4
+
+vim.o.mouse = 'a'
+
+vim.o.scrolloff = 8
+vim.o.signcolumn = 'yes'
+
+vim.o.clipboard = 'unnamedplus'
+
+vim.o.breakindent = true
+
+vim.o.swapfile = false
+vim.o.backup = false
+vim.o.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.o.undofile = true
+
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+vim.o.updatetime = 50
+vim.o.timeoutlen = 300
+
+vim.o.completeopt = 'menuone,noselect'
+
+vim.o.termguicolors = true
